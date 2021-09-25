b5ab2e020384db2a3cb4ef9596549e515c1bece8
Hoang Nguyen <nguyenvanhoang26041994@gmail.com>
2021-09-25 21:20:07 +0700

    AvatarGroup TS

---
+++ src/core/AvatarGroup/AvatarGroup.d.ts
@@ -0,0 +1,4 @@
+export interface AvatarGroupProps extends React.HTMLProps<HTMLDivElement> {};
+
+declare const AvatarGroup: (props: AvatarGroupProps) => JSX.Element;
+export default AvatarGroup;
--- src/core/index.d.ts
+++ src/core/index.d.ts
@@ -1,7 +1,7 @@
 // export { default as Alert } from './Alert';
 // export { default as Animated } from './Animated';
 export { default as Avatar } from './Avatar';
-// export { default as AvatarGroup } from './AvatarGroup';
+export { default as AvatarGroup } from './AvatarGroup';
 export { default as Badge } from './Badge';
 export { default as Button } from './Button';
 export { default as ButtonGroup } from './ButtonGroup';
