7506beb6854435d8ec0e496518ad383b740cc2be
Hoang Nguyen <nguyenvanhoang26041994@gmail.com>
2021-09-25 21:23:08 +0700

    Fix typo

--- core/AvatarGroup/AvatarGroup.d.ts
+++ core/AvatarGroup/AvatarGroup.d.ts
@@ -1,4 +1,4 @@
-export interface AvatarGroupProps extends React.HTMLProps<HTMLDivElement> {};
+export interface AvatarGroupProps extends React.HTMLProps<HTMLDivElement> {}
 
 declare const AvatarGroup: (props: AvatarGroupProps) => JSX.Element;
 export default AvatarGroup;
